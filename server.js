bd105ecb69067f06630a81e3d5adc2f298e9e5b5
Tom Schrimshaw <tschrimshaw@gmail.com>
2020-03-29 18:09:48 +0100

    add server code to create and run localhost

--- server.js
+++ server.js
@@ -2,21 +2,29 @@
 // Express to run server and routes
 
 // Start up an instance of app
-
+const express = require('express');
+const app = express();
 /* Dependencies */
 /* Middleware*/
 
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
-
+app.use(express.static('website'));
 // Spin up the server
 // Callback to debug
+const port = 3000;
+const server = app.listen(port, () => {
+  console.log(`server running on localhost: ${port}`);
+});
 
 // Initialize all route with a callback function
 
 // Callback function to complete GET '/all'
 
 // Post Route
-  
