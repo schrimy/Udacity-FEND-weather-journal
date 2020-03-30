92e28f21f7cc6f68826470117bf77a1ccbcbf23c
Tom Schrimshaw <tschrimshaw@gmail.com>
2020-03-30 13:05:45 +0100

    add globals for api key / base url

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 // Personal API Key for OpenWeatherMap API
-
+const apiKey = '80eaf3db70bd995b99c4d1224eb5278a';
+const baseUrl = 'api.openweathermap.org/data/2.5/weather?zip=';
 // Event listener to add function to existing HTML DOM element
 
 /* Function called by event listener */
